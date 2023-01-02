38aaef89f7f1db3605b91bef1f9c2b2250a4e251
Vladimir83 <vovaf@bk.ru>
2023-01-02 19:35:40 +0300

    task62

---
+++ task62/Program.cs
@@ -0,0 +1,31 @@
+﻿static void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write(String.Format("{0,4:00.#}", arr[i, j]));
+        Console.WriteLine();
+    }
+}
+
+int side = 4;
+
+int[,] arr = new int[side, side];
+int idx = 0;
+
+for (int x = 0; (x <= side / 2); x++)
+{
+    for (int i = x; i < side - x; i++)
+        arr[x, i] = ++idx;
+
+    for (int i = x + 1; i < side - x; i++)
+        arr[i, side - 1 - x] = ++idx;
+
+    for (int i = side - 2 - x; i >= 0 + x; i--)
+        arr[side - 1 - x, i] = ++idx;
+
+    for (int i = side - 2 - x; i > 0 + x; i--)
+        arr[i, x] = ++idx;
+}
+
+PrintArray(arr);
